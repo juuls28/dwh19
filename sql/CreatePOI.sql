69f7dca23729fadf0c3e3b2862ccbf896f10b8a2
jseiffer <julius.seiffert@stud.htwk-leipzig.de>
2019-09-13 20:20:08 +0200

    Added analysis project

--- sql/CreatePOI.sql
+++ sql/CreatePOI.sql
@@ -2,7 +2,14 @@ Drop Table [DWH19-05].[dbo].[Point_Of_Interest];
 
 Create Table [DWH19-05].[dbo].[Point_Of_Interest] (
 	ID int IDENTITY(1,1) Primary Key,
-	Name varchar,
-	Geom varchar,
-	Address varchar
+	Name varchar(100),
+	Geom varchar(50),
+	Latitude varchar(20),
+	Longitude varchar(20)
 );
+
+Update p1
+Set Latitude = SUBSTRING(p1.Geom, 8, 18),
+	Longitude = SUBSTRING(p1.Geom, 26, (CHARINDEX(')', p1.Geom) - 26))
+From Point_Of_Interest as p1
+Inner Join Point_Of_Interest as p2 ON p1.ID = p2.ID
